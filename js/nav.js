e33c50a534e94c62069fff36467c556d4dbaabca
MatthewJDavison <MatthewJDavison1@outlook.com>
2021-05-03 16:11:24 +0100

    Fixed nav issue.

---
+++ js/nav.js
@@ -0,0 +1,6 @@
+const navToggle = document.querySelector('.menu-button');
+
+navToggle.addEventListener('click', _ => {
+    document.body.classList.toggle('nav-is-open');
+    console.log("test");
+});
